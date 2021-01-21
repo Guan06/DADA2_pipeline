c2d3863309024c07f799851ab43e9874daa8d9a7
Your Name <you@example.com>
2021-01-21 18:59:00 +0100

    modify config.sh

---
+++ config.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# config file for amplicon data analysis
+# guan@mpipz.mpg.de, modified from garridoo@mpipz.mpg.de
+
+## set the directory for the results to be saved
+working_dir="/netscratch/somewhere"
+
+## set the directory where the sequencing data is stored
+data_dir="/biodata/somewhere"
+
+## define the profiled kingdom
+bacteria=false
+
+fungi=true
+
+## for ITS2
+fungi_FWD="GTGARTCATCGAATCTTTG"
+fungi_REV="TCCTCCGCTTATTGATATGC"
+
+## for ITS1
+#fungi_FWD="CTTGGTCATTTAGAGGAAGTAA"
+#fungi_REV="GCTGCGTTCTTCATCGATGC"
+
+oomycetes=false
+
+## for ITS1o
+oomycetes_FWD="CGGAAGGATCATTACCAC"
+oomycetes_REV="AGCCTAGACATCCACTGCTG"
+
+## set the list of sequencing data
+## e.g. 001, should be the same as "prefix" in README.md
+l_list_miseq="001"

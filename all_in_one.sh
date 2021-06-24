432bf1079edb330adde2e097b24d42cb8db8d07d
Your Name <you@example.com>
2021-06-24 17:52:30 +0200

    update readme and all_in_on

--- all_in_one.sh
+++ all_in_one.sh
@@ -1,3 +1,9 @@
-sh step1_demultiplex.sh
-sh step2_dada2.sh
-sh step3_get_ASV_table.sh
+#!/bin/bash
+
+set -e
+
+scripts_dir=`dirname $0`
+
+$scripts_dir/step1_demultiplex.sh
+$scripts_dir/step2_dada2.sh
+$scripts_dir/step3_get_ASV_table.sh
